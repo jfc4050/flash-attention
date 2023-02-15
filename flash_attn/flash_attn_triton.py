fa650914d6a765d12358e0f2856ead9aa2ac98a4
Justin Chiu <jfc4050@gmail.com>
2023-02-15 21:19:37 +0000

    move p_dropped computation
    [----------------------------------------------------- fwd -----------------------------------------------------]
                                                            |  Flash (CUDA)  |  Flash (Triton)  |  Standard Attention
    48 threads: -----------------------------------------------------------------------------------------------------
          (1,12,4096,128), p=0.0, bias=False, causal=False  |     1722.5     |       925.4      |        1718.0
          (1,12,4096,128), p=0.0, bias=False, causal=True   |      732.3     |       548.3      |        3014.9
          (1,12,4096,128), p=0.1, bias=False, causal=False  |     1577.6     |      1201.6      |        2572.3
          (1,12,4096,128), p=0.1, bias=False, causal=True   |      779.9     |       715.6      |        3875.3
          (1,12,4096,128), p=0.0, bias=True, causal=False   |                |       903.2      |        2589.3
          (1,12,4096,128), p=0.0, bias=True, causal=True    |                |       572.7      |        3895.6
          (1,12,4096,128), p=0.1, bias=True, causal=False   |                |      1180.4      |        3448.5
          (1,12,4096,128), p=0.1, bias=True, causal=True    |                |       731.0      |        4753.4
    
    Times are in microseconds (us).
    
    [----------------------------------------------------- bwd -----------------------------------------------------]
                                                            |  Flash (CUDA)  |  Flash (Triton)  |  Standard Attention
    48 threads: -----------------------------------------------------------------------------------------------------
          (1,12,4096,128), p=0.0, bias=False, causal=False  |      3.5       |       3.4        |         3.7
          (1,12,4096,128), p=0.0, bias=False, causal=True   |      1.7       |       2.2        |         8.1
          (1,12,4096,128), p=0.1, bias=False, causal=False  |      3.3       |       3.8        |         4.4
          (1,12,4096,128), p=0.1, bias=False, causal=True   |      1.7       |       2.3        |         8.8
          (1,12,4096,128), p=0.0, bias=True, causal=False   |                |       3.5        |         3.7
          (1,12,4096,128), p=0.0, bias=True, causal=True    |                |       2.3        |         5.3
          (1,12,4096,128), p=0.1, bias=True, causal=False   |                |       4.0        |         4.4
          (1,12,4096,128), p=0.1, bias=True, causal=True    |                |       2.3        |         6.1
    
    Times are in milliseconds (ms).
    
    [--------------------------------------------------- fwd+bwd ---------------------------------------------------]
                                                            |  Flash (CUDA)  |  Flash (Triton)  |  Standard Attention
    48 threads: -----------------------------------------------------------------------------------------------------
          (1,12,4096,128), p=0.0, bias=False, causal=False  |      5.1       |       4.3        |          5.5
          (1,12,4096,128), p=0.0, bias=False, causal=True   |      2.5       |       2.8        |         11.1
          (1,12,4096,128), p=0.1, bias=False, causal=False  |      4.9       |       5.1        |          7.1
          (1,12,4096,128), p=0.1, bias=False, causal=True   |      2.5       |       3.0        |         12.7
          (1,12,4096,128), p=0.0, bias=True, causal=False   |                |       4.5        |          6.3
          (1,12,4096,128), p=0.0, bias=True, causal=True    |                |       2.9        |          9.3
          (1,12,4096,128), p=0.1, bias=True, causal=False   |                |       5.2        |          7.9
          (1,12,4096,128), p=0.1, bias=True, causal=True    |                |       3.0        |         10.9

--- flash_attn/flash_attn_triton.py
+++ flash_attn/flash_attn_triton.py
@@ -440,12 +440,6 @@ def _bwd_kernel_one_col_block(
             # elementwise multiplication by Zij.
             dropout_mask = make_dropout_mask(dropout_p, rng_seed, indices)
 
-        if USE_DROPOUT:
-            # Pij_dropped = Pij * Zij
-            p_dropped = tl.where(dropout_mask, p * (1.0 / (1.0 - dropout_p)), 0.0)
-        else:
-            p_dropped = p
-
         # compute dv
         # [2022-10-30] TD: A Triton bug: if EVEN_M=True and EVEN_HEADDIM=False, if we call
         # do = tl.load(do_ptrs, mask=offs_d[None, :] < headdim, other=0.0), we get wrong outputs
@@ -459,7 +453,11 @@ def _bwd_kernel_one_col_block(
             do = tl.load(do_ptrs, mask=(offs_m_curr[:, None] < seqlen_q)
                                         & (offs_d[None, :] < headdim), other=0.0)
 
-        dv += tl.dot(p_dropped.to(do.dtype), do, trans_a=True).to(dv.dtype)
+        if USE_DROPOUT:
+            p_dropped = tl.where(dropout_mask, p * (1.0 / (1.0 - dropout_p)), 0.0)
+            dv += tl.dot(p_dropped.to(do.dtype), do, trans_a=True).to(dv.dtype)
+        else:
+            dv += tl.dot(p.to(do.dtype), do, trans_a=True).to(dv.dtype)
 
         # compute dp = dot(v, do)
         # There seems to be a race condition when headdim=48/96, and dq, dk are wrong.
