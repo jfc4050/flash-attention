72bf7e5dd1a34202b3175bee32ba521c832f8c63
Justin Chiu <jfc4050@gmail.com>
2023-02-28 17:08:00 +0000

    add run once test script

---
+++ run_triton_once.py
@@ -0,0 +1,37 @@
+import torch
+from flash_attn.flash_attn_triton import flash_attn_func
+
+batch_size = 1
+seq_len = 2048
+n_heads = 12
+head_dim = 128
+device = "cuda"
+dtype = torch.bfloat16
+
+# setup inputs
+q = torch.randn(
+    (batch_size, seq_len, n_heads, head_dim),
+    requires_grad=True,
+    device=device,
+    dtype=dtype,
+)
+k = torch.randn(
+    (batch_size, seq_len, n_heads, head_dim),
+    requires_grad=True,
+    device=device,
+    dtype=dtype,
+)
+v = torch.randn(
+    (batch_size, seq_len, n_heads, head_dim),
+    requires_grad=True,
+    device=device,
+    dtype=dtype,
+)
+bias = None
+causal = True
+dropout_p = 0.0
+
+out = flash_attn_func(q, k, v, bias, causal, dropout_p)
+
+grad_out = torch.rand_like(q)
+out.backward(grad_out)
